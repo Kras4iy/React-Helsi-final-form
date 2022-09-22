fe6bf5f0ff2b52fb25ff07b9a65639ab2c6671b7
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-09-22 17:08:27 +0300

    Last hard point

--- src/components/PersonForm/PersonForm.jsx
+++ src/components/PersonForm/PersonForm.jsx
@@ -5,7 +5,7 @@ import { Form, Field } from 'react-final-form';
 import { Input } from '../Input/index';
 import './PersonForm.scss';
 
-const createField = (name, isRequired = false, type = 'text') => (
+const createInput = (name, isRequired = false, type = 'text') => (
   {
     name,
     isRequired,
@@ -13,14 +13,39 @@ const createField = (name, isRequired = false, type = 'text') => (
   }
 );
 
-const fields = {
-  secondName: createField('Прізвище', true),
-  firstName: createField('Ім`я', true),
-  thirdName: createField('По батькові'),
-  RNOKPP: createField('РНОКПП(ІПН)'),
-  birthday: createField('Дата народження', false, 'date'),
-  sex: createField('Стать', true, 'select'),
+const createSelect = (name, isRequired = false, values = []) => (
+  {
+    name,
+    isRequired,
+    values,
+    type: 'select',
+  }
+);
+
+const firstSection = {
+  secondName: createInput('Прізвище', true),
+  firstName: createInput('Ім`я', true),
+  thirdName: createInput('По батькові'),
+  RNOKPP: createInput('РНОКПП(ІПН)'),
+  birthday: createInput('Дата народження', false, 'date'),
+  sex: createSelect('Стать', true, ['Чоловік', 'Жінка']),
 };
+
+const secondSection = {
+  country: createInput('Країна народження', true),
+  city: createInput('Місце народження', true),
+  preferences: createSelect(
+    'Бажаний спосіб зв`язку з пацієнтом',
+    false,
+    ['Електронною поштою',
+      'Телефоном'],
+  ),
+  secretWord: createInput('Секретне слово', true),
+  phoneNumber: createInput('Контактний номер телефону', false),
+  email: createInput('Aдреса електронної пошти', false),
+};
+
+const onLong = value => (value ? undefined : 'Required');
 
 export const PersonForm = () => {
   const onSubmit = (values) => {
@@ -49,38 +74,92 @@ export const PersonForm = () => {
       render={({ handleSubmit }) => (
         <form onSubmit={handleSubmit} className="form">
           <h2>Simple Default Input</h2>
-          <div>
-            {/* <Field name="firstName">
-              {({ input, meta }) => (
-                <div className="form__field">
-                  <label className="form__label">FirstName</label>
-                  <input
-                    {...input}
-                    type="text"
-                    placeholder="UserName"
-                    className="form__input"
-                  />
-                  <p className="form__error">{meta.error}</p>
-                </div>
-              )}
-            </Field> */}
+          <section className="form__block">
+            {Object.keys(firstSection).map((key) => {
+              switch (firstSection[key].type) {
+                case 'select':
+                  console.log(firstSection[key].values);
 
-            {Object.keys(fields).map(field => (
-              <Field name={field} key={field}>
-                {({ input, meta }) => (
-                  <div className="form__field">
-                    <Input
-                      {...input}
-                      label={fields[field].name}
-                      isRequired={fields[field].isRequired}
-                      type={fields[field].type}
-                    />
-                  </div>
-                )}
-              </Field>
-            ))}
+                  return (
+                    <Field
+                      name={key}
+                      key={key}
+                      component={firstSection[key].type}
+                    >
+                      <option> -Вибрати- </option>
+                      {firstSection[key].values.map(value => (
+                        <option key={value}>{value}</option>))}
+                    </Field>
+                  );
 
-          </div>
+                default:
+                  return (
+                    <Field name={key} key={key} validate={onLong}>
+                      {({ input, meta }) => (
+                        <div className="form__field">
+                          <Input
+                            {...input}
+                            label={firstSection[key].name}
+                            isRequired={firstSection[key].isRequired}
+                            type={firstSection[key].type}
+                          />
+                          {meta.error && meta.touched
+                      && (
+                        <p className="form__error">
+                          {meta.error}
+                        </p>
+                      )}
+                        </div>
+                      )}
+                    </Field>
+                  );
+              }
+            })}
+
+          </section>
+
+          <section className="form__block">
+            {Object.keys(secondSection).map((key) => {
+              switch (secondSection[key].type) {
+                case 'select':
+                  console.log(secondSection[key].values);
+
+                  return (
+                    <Field
+                      name={key}
+                      key={key}
+                      component={secondSection[key].type}
+                    >
+                      <option> -Вибрати- </option>
+                      {secondSection[key].values.map(value => (
+                        <option key={value}>{value}</option>))}
+                    </Field>
+                  );
+
+                default:
+                  return (
+                    <Field name={key} key={key} validate={onLong}>
+                      {({ input, meta }) => (
+                        <div className="form__field">
+                          <Input
+                            {...input}
+                            label={secondSection[key].name}
+                            isRequired={secondSection[key].isRequired}
+                            type={secondSection[key].type}
+                          />
+                          {meta.error && meta.touched
+                      && (
+                        <p className="form__error">
+                          {meta.error}
+                        </p>
+                      )}
+                        </div>
+                      )}
+                    </Field>
+                  );
+              }
+            })}
+          </section>
 
           <button type="submit">Submit</button>
         </form>
